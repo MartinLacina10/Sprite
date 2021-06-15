bdb89e5917ac0332a566f31095d8a94194363edf
MartinLacina10 <58588480+MartinLacina10@users.noreply.github.com>
2021-06-15 08:47:36 +0200

    1
    1

---
+++ src/main/java/educanet/Game.java
@@ -0,0 +1,171 @@
+package educanet;
+
+
+import org.joml.Matrix4f;
+import org.lwjgl.BufferUtils;
+import java.nio.ByteBuffer;
+import org.lwjgl.opengl.GL33;
+import org.lwjgl.stb.STBImage;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
+
+public class Game {
+
+    static private int squareVaoId;
+    static private int textureIndicesId;
+    static private int uniformMatrixLocation;
+    static private int textureId;
+    static private final FloatBuffer bfr = BufferUtils.createFloatBuffer(8);
+    static float index = 0f;
+
+    static private final float[] vertices = {
+            0.2f, 0.3f, 0.0f, //right top
+            0.2f, -0.3f, 0.0f, //right bottom
+            -0.2f, -0.3f, 0.0f, //left bottom
+            -0.2f, 0.3f, 0.0f,//left top
+    };
+    static private float[] textures = {
+            1.0f, 0.0f,
+            1.0f, 1.0f,
+            0.0f, 1.0f,
+            0.0f, 0.0f,
+    };
+
+    static private final float[] colors = {
+            1.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+    };
+
+    static private final int[] indices = {
+            0, 1, 3, // First triangle
+            1, 2, 3 // Second triangle
+    };
+
+    static private final Matrix4f matrix = new Matrix4f()
+            .identity();
+    static private final FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16);
+
+
+    static int squareVboId = GL33.glGenBuffers();
+    static int squareEboId = GL33.glGenBuffers();
+    static int colorsId = GL33.glGenBuffers();
+
+    public static void init() {
+        Shaders.initShaders();
+        loadImage();
+
+        int uniformColorLocation = GL33.glGetUniformLocation(Shaders.shaderProgramId, "outColor");
+        uniformMatrixLocation = GL33.glGetUniformLocation(Shaders.shaderProgramId, "matrix");
+        squareVaoId = GL33.glGenVertexArrays();
+        textureIndicesId = GL33.glGenBuffers();
+        textureId = GL33.glGenTextures();
+
+        GL33.glBindVertexArray(squareVaoId);
+
+        GL33.glBindBuffer(GL33.GL_ELEMENT_ARRAY_BUFFER, squareEboId);
+        IntBuffer ib = BufferUtils.createIntBuffer(indices.length)
+                .put(indices)
+                .flip();
+        GL33.glBufferData(GL33.GL_ELEMENT_ARRAY_BUFFER, ib, GL33.GL_STATIC_DRAW);
+
+        GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, squareVboId);
+
+        FloatBuffer fb = BufferUtils.createFloatBuffer(vertices.length)
+                .put(vertices)
+                .flip();
+
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, fb, GL33.GL_STATIC_DRAW);
+        GL33.glVertexAttribPointer(0, 3, GL33.GL_FLOAT, false, 0, 0);
+        GL33.glEnableVertexAttribArray(0);
+
+
+        GL33.glUseProgram(Shaders.shaderProgramId);
+        GL33.glUniform3f(uniformColorLocation, 1.0f, 0.0f, 0.0f);
+
+        matrix.get(matrixBuffer);
+        GL33.glUniformMatrix4fv(uniformMatrixLocation, false, matrixBuffer);
+
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, fb, GL33.GL_STATIC_DRAW);
+        GL33.glVertexAttribPointer(0,3, GL33.GL_FLOAT, false, 0, 0);
+        GL33.glEnableVertexAttribArray(0);
+
+        GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, colorsId);
+
+        FloatBuffer cb = BufferUtils.createFloatBuffer(colors.length)
+                .put(colors)
+                .flip();
+
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, cb, GL33.GL_STATIC_DRAW);
+        GL33.glVertexAttribPointer(1,3, GL33.GL_FLOAT, false, 0, 0);
+        GL33.glEnableVertexAttribArray(1);
+
+        GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, textureIndicesId);
+
+        FloatBuffer tb = BufferUtils.createFloatBuffer(textures.length)
+                .put(textures)
+                .flip();
+
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, tb, GL33.GL_STATIC_DRAW);
+        GL33.glVertexAttribPointer(2, 2, GL33.GL_FLOAT, false, 0, 0);
+        GL33.glEnableVertexAttribArray(2);
+
+        MemoryUtil.memFree(fb);
+    }
+
+
+    public static void update() {
+        changeSpritePos();
+
+        GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, textureIndicesId);
+
+        bfr.clear()
+				.put(textures)
+				.flip();
+
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, bfr, GL33.GL_STATIC_DRAW);
+        GL33.glVertexAttribPointer(2, 2, GL33.GL_FLOAT, false, 0, 0);
+        GL33.glEnableVertexAttribArray(2);
+    }
+
+    public static void render() {
+        GL33.glUniformMatrix4fv(uniformMatrixLocation, false, matrixBuffer);
+        GL33.glUseProgram(Shaders.shaderProgramId);
+        GL33.glBindVertexArray(squareVaoId);
+        GL33.glDrawElements(GL33.GL_TRIANGLES, vertices.length, GL33.GL_UNSIGNED_INT, 0);
+    }
+
+    public static void changeSpritePos(){
+        textures[0] = (index+1)/6;
+        textures[2] = (index+1)/6;
+        textures[4] = index/6;
+        textures[6] = index/6;
+        index++;
+        if(index==6){
+            index=0;
+        }
+    }
+
+    public static void loadImage() {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer w = stack.mallocInt(1);
+            IntBuffer h = stack.mallocInt(1);
+            IntBuffer comp = stack.mallocInt(1);
+
+            ByteBuffer img = STBImage.stbi_load("res/img.png", w, h, comp, 3);
+            if (img != null) {
+                img.flip();
+
+                GL33.glBindTexture(GL33.GL_TEXTURE_2D, textureId);
+                GL33.glTexImage2D(GL33.GL_TEXTURE_2D, 0, GL33.GL_RGB, w.get(), h.get(), 0, GL33.GL_RGB, GL33.GL_UNSIGNED_BYTE, img);
+                GL33.glGenerateMipmap(GL33.GL_TEXTURE_2D);
+
+                STBImage.stbi_image_free(img);
+            }
+        }
+    }
+
+}
